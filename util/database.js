2e52de1fb07c118d517e4777de93824994cebf4f
korneel <korneel.loy@gmail.com>
2025-06-02 19:28:26 +0200

    error handling connection db

--- util/database.js
+++ util/database.js
@@ -2,11 +2,19 @@ const mysql = require('mysql2');
 
 const config = require('../config/config.json');
 
-const pool = mysql.createPool({
-  host: config.host,
-  user: config.user,
-  database: config.database,
-  password: config.password
-});
+let pool;
+
+try {
+  pool = mysql.createPool({
+    host: config.host,
+    user: config.user,
+    database: config.database,
+    password: config.password
+  });
+  console.log('Connected to database.');
+} catch(err) {
+  console.error('Could not connect to the database:', err.message);
+  process.exit(1);
+}
 
 module.exports = pool.promise();
